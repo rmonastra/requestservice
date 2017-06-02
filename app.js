3cd43dd7d81066f3d62e6fb801df0e4dad45ed8b
Richard Monastra <rmonastra2@gmail.com>
2017-06-02 01:22:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//Dependencies the app.js will use to perform functions, list can be found in the package.json file.
+var express = require('express'); //NodeJs framework that allows you to build APIs
+var bodyParser = require('body-parser'); //reads HTTP POST data
+var cors = require('cors'); //allows Cross-origin resource sharing(cors)
+var useragent = require('express-useragent'); //module to collect web browser, operating system and other data
+
+//creates the app
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(useragent.express());
+
+//API
+var api = '/api/whoami';
+//Function & variables to retrieve user info
+app.get(api, function(req, res, next) {
+    var language = req.acceptsLanguages();
+    var software = 'Operating System: ' + req.useragent.os + ', Browser: ' + req.useragent.browser;
+    var ip = req.ip;
+    //responds with data in JSON format
+    res.json({ 'ip': ip, 'language': language[0], 'software': software })
+});
+//path for connection
+app.listen(3000, function() {
+    console.log("Listening on port: 3000");
+});
+//allows this code/file to be used by other files
+module.exports = app;
